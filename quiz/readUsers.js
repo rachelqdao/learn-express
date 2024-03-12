d1575122c975d19ceb9e9d5caf5e194a34bf74c5
Rachel Dao <rachelqdao@gmail.com>
2024-03-12 12:19:21 -0400

    fixed readUsers / writeUsers

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,28 @@
 const express = require('express')
 const router = express.Router();
+
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+router.get('/usernames', (req, res) => {
+    let usernames = req.users.map(function(user) {
+      return {id: user.id, username: user.username};
+    });
+    res.send(usernames);
+  });
+
+
+router.get('/username/:name', (req, res) => {
+    let name = req.params.name;
+    let users = req.users.filter(user => user.username === name)
+  
+    if (users.length === 0) {
+      return res.status(404).send('User not found')
+    }
+  
+    res.send(users)
+  })
+
+module.exports = router
